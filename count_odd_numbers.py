34ccff7b1557fa0bea98b351a3757b80f86ce961
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-13 00:24:55 +0800

    pseudocode

---
+++ count_odd_numbers.py
@@ -0,0 +1,10 @@
+# Prog08: Create a program that ask user to input 10 numbers. Print how many are odd numbers.
+
+# ask user for 10 input numbers
+    # initialize variables
+    # loop to ask the user 10 times
+ # check if the input is odd number
+    # increment variable
+    
+#print output
+    
