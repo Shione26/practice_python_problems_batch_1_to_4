6eb9b5277787d665c24d2a7e46225d65194afd0e
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-12 23:20:49 +0800

    pseudocode

---
+++ print_product.py
@@ -0,0 +1,5 @@
+# Prog04: Create a program that ask user to input 2 numbers. Print the product of the two numbers.
+
+# ask user for inputs of two numbers
+# multiply the two numbers
+# print the product of the two numbers
