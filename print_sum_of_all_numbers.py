c8a108d85c5f193882b0fb6bff06f8ecb7374a4d
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-13 00:08:55 +0800

    ask inputs

--- print_sum_of_all_numbers.py
+++ print_sum_of_all_numbers.py
@@ -2,6 +2,12 @@
 
 # ask user for 10 imputs
     # initialize variables before loop
+
+number_count = 0
+total_sum = 0
     # inside for loop, ask the user for inputs 10 times
+for number_count in range(10):
+    number = int(input(f"Enter number {number_count + 1}: "))
+
 # add all the inputs
 # print sum
