02a196704da5b3a0991dbf85105c2b2a5847a3a0
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-12 23:09:24 +0800

    print equal

--- print_equal.py
+++ print_equal.py
@@ -1,3 +1,5 @@
+# Prog02: Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
+
 # ask the user to input two numbers
 
 num1 = int(input("Enter a number: "))
@@ -5,3 +7,6 @@ num2 = int(input("Enter a number: "))
 
 # check if the numbers are the same
     # print Equal
+
+if num1 == num2:
+    print("Equal")
