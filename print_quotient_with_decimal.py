78cf81819947d60a65bfdffdcbb274315bf66e78
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-12 23:28:59 +0800

    ask inputs

--- print_quotient_with_decimal.py
+++ print_quotient_with_decimal.py
@@ -1,5 +1,8 @@
 # Prog05: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point
 
 # ask user for inputs of two numbers
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter a number: "))
+
 # divide the two numbers
 # print the quotient with the decimal point
