c5a80043b89589f733e8c5a0abd3d121856683fd
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-12 23:22:43 +0800

    ask inputs

--- print_product.py
+++ print_product.py
@@ -1,5 +1,9 @@
 # Prog04: Create a program that ask user to input 2 numbers. Print the product of the two numbers.
 
 # ask user for inputs of two numbers
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter a number: "))
+
 # multiply the two numbers
 # print the product of the two numbers
+
