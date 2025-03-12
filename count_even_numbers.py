aa689deb46b1db7939546c0b064fcc2218126e90
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-12 23:49:58 +0800

    pseudocode

---
+++ count_even_numbers.py
@@ -0,0 +1,9 @@
+# Prog07: Create a program that ask user to input 10 numbers. Print how many are even numbers.
+
+# ask user for 10 numbers
+    # initialize variable before loop
+    # inside for loop, ask the user for inputs 10 times
+    # increment variable 
+# check if the input is an even number
+    # increment variable 
+# print output
