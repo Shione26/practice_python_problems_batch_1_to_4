7e4f199c61bbeb07227f6d1445ca7c2b66e5a10e
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-13 00:06:20 +0800

    pseudocode

---
+++ print_sum_of_all_numbers.py
@@ -0,0 +1,7 @@
+# Prog07: Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
+
+# ask user for 10 imputs
+    # initialize variables before loop
+    # inside for loop, ask the user for inputs 10 times
+# add all the inputs
+# print sum
