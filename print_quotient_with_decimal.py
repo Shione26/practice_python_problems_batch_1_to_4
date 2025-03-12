405b89d81c7d0e37542b71d8a83ccbe0c0ac9da0
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-12 23:27:31 +0800

    pseudocode

---
+++ print_quotient_with_decimal.py
@@ -0,0 +1,5 @@
+# Prog05: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point
+
+# ask user for inputs of two numbers
+# divide the two numbers
+# print the quotient with the decimal point
