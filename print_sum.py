fbb08ce0d720cdf73f3539ed7b423db225a40a2c
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-12 23:16:05 +0800

    ask inputs

--- print_sum.py
+++ print_sum.py
@@ -1,5 +1,9 @@
 # Prog03: Create a program that ask user to input 2 numbers. Print the sum of the two numbers.
 
 # ask the user for inputs of 2 numbers
+
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter a number: "))
+
 # add the two numbers
 # print the sum
