eb7680df01a79c2b9981ffac77aa665515972f13
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-12 22:51:18 +0800

    ask inputs of two numbers

---
+++ print_bigger_number.py
@@ -0,0 +1,7 @@
+# ask the user for input of 2 numbers
+
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter a number: "))
+# check which number is bigger
+    # print the bigger number
+
