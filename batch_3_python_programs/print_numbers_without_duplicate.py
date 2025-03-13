c0fbc064103fcbdca64f6cdb1959721d0f3d7f7a
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-13 21:43:21 +0800

    pseudocode

---
+++ batch_3_python_programs/print_numbers_without_duplicate.py
@@ -0,0 +1,5 @@
+# Prog01: Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
+
+# ask the user to input 10 numbers
+# identify which numbers are inputted only once
+# print the numbers with no duplicate
