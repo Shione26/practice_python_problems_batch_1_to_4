94e69ae396622fc4d55d7eb03c0f4beae9dead39
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-13 21:48:17 +0800

    ask for 10 inputs

--- batch_3_python_programs/print_numbers_without_duplicate.py
+++ batch_3_python_programs/print_numbers_without_duplicate.py
@@ -1,5 +1,10 @@
 # Prog01: Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
 
 # ask the user to input 10 numbers
+
+
+for number_count in range(10):
+    number = int(input(f"Enter number {number_count + 1}: "))
+
 # identify which numbers are inputted only once
 # print the numbers with no duplicate
