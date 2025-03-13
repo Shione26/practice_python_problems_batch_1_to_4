0931c3f52f877a51fc1558b5e27b6d6ba662b258
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-13 14:15:10 +0800

    pseudocode

---
+++ batch_2_python_programs/print_not_equal.py
@@ -0,0 +1,5 @@
+# Prog02: Create a program that ask user to input 2 numbers. Print "Not Equal" when the numbers are not the same.
+
+# ask user for inputs of two numbers
+# check if the two numbers are not the same
+# print not equal
