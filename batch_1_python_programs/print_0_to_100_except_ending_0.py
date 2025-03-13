d72ae2e59682b41e3d333f4743e8e7025a8127ca
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-13 20:31:57 +0800

    pseudocode

---
+++ batch_1_python_programs/print_0_to_100_except_ending_0.py
@@ -0,0 +1,5 @@
+# Prog10: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero.
+
+# print 0-100
+# check condition if number is ending in zero
+# print output
