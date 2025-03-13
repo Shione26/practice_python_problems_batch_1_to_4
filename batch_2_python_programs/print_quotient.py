faa6d221da92988e0f84945f54755e63e5de5654
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-13 17:46:39 +0800

    ask for inputs then divide then print output

--- batch_2_python_programs/print_quotient.py
+++ batch_2_python_programs/print_quotient.py
@@ -1,5 +1,11 @@
 # Prog04: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers without the decimal point
 
 # ask the user for input of two numbers
+
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter a number: "))
+
 # divide the two numbers
 # print the quotient
+quotient = num1 // num2
+print(int(quotient))
