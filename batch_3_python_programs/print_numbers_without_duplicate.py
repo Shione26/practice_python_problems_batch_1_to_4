bacddc8a771163f806524c69e7de4d766d557e06
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-13 21:57:32 +0800

    store the input into the empty list then check condition then print output

--- batch_3_python_programs/print_numbers_without_duplicate.py
+++ batch_3_python_programs/print_numbers_without_duplicate.py
@@ -1,10 +1,11 @@
 # Prog01: Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
 
 # ask the user to input 10 numbers
-
+numbers = []
 
 for number_count in range(10):
     number = int(input(f"Enter number {number_count + 1}: "))
+    numbers.append(number)
 
 # identify which numbers are inputted only once
 # print the numbers with no duplicate
