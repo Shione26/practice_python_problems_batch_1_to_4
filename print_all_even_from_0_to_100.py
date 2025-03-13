5d7658be6708f63f8f926509b7da3b6eb7771164
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-13 11:45:55 +0800

    moved files to folder

--- print_all_even_from_0_to_100.py
+++
@@ -1,8 +0,0 @@
-# Prog09: Create a program that print all the even numbers starting from 0 to 100. (Use for loop)
-
-# print numbers 1 to 100
-for number in range(0, 101):
-
-# check if even number
-    if number % 2 == 0:
-            print(number)     # print if true
