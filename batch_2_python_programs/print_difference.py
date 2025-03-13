5b7ebc909fa41cf85be522e5f0c3e40a4df2a3da
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-13 17:30:59 +0800

    print total

--- batch_2_python_programs/print_difference.py
+++ batch_2_python_programs/print_difference.py
@@ -1,5 +1,10 @@
 # Prog03: Create a program that ask user to input 2 numbers. Print the difference of the two numbers.
 
 # ask the user for inputs of 2 numbers
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter a number: "))
+
 # subtract the two numbers
 # print the difference
+total = num1 - num2
+print(total)
