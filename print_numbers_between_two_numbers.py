0e034dcec85d043f3badbaf6fbcaf75759317b45
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-13 11:36:01 +0800

    pseudocode

---
+++ print_numbers_between_two_numbers.py
@@ -0,0 +1,5 @@
+# Prog10: Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers.
+
+# ask user for inputs of two numbers
+# get all numbers in between two numbers
+# print output
