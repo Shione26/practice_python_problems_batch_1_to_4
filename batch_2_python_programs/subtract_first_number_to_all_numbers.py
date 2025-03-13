c5432dcf2d47909d8c69d0bcf7b48c0152469ae5
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-13 19:49:55 +0800

    update: ask for the first number then subtract the rest 9 inputs and print output

--- batch_2_python_programs/subtract_first_number_to_all_numbers.py
+++ batch_2_python_programs/subtract_first_number_to_all_numbers.py
@@ -1,12 +1,13 @@
 # Prog06: Create a program that ask user to input 10 numbers. Print the result of the first number minus all of the remaining numbers.
 
 # ask user for 10 imputs
-# initialize variable 
-total = 0
+# ask for the first number 
+total = int(input("Enter number 1: "))
 
-# loop to ask user for 10 inputs
-for number_count in range(10):
+# loop to ask user for the rest of the inputs
+for number_count in range(1, 10):
     number = int(input(f"Enter number {number_count + 1}: "))
+    total -= number    # subtract every inputted number from the difference in each loop iteration
     
-    # subtract every inputted number from the difference in each loop iteration
 # print the result
+print(total)
