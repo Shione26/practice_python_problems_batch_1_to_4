aaf7b78043e963608b7f26d51da341e906b9407a
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-13 20:23:15 +0800

    print 0-100

--- batch_2_python_programs/print_0_to_100_except_0_or_5.py
+++ batch_2_python_programs/print_0_to_100_except_0_or_5.py
@@ -1,5 +1,8 @@
 # Prog09: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero or ending five.
 
 # print 0-100
+
+for number in range(0, 100):
+    print(number)
 # check condition if ending zero or 5
 # print output
