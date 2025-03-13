4316bf132b6515694a1a10a81adbf31a810afd79
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-13 17:51:42 +0800

    pseudocode

---
+++ batch_2_python_programs/print_remainder.py
@@ -0,0 +1,5 @@
+# Prog05: Create a program that ask user to input 2 numbers. Print the remainder when the first number is divided by the second number.
+
+# ask for input of 2 numbers
+# have first number divided by the second number
+# print remainder
