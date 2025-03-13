184e93eddc05f60e836c2d8cc7666ea052ac4f6f
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-13 20:19:06 +0800

    updated pseudocode and loop then increment by 2 then print output

--- batch_2_python_programs/print_all_odd_from_0_to_100.py
+++ batch_2_python_programs/print_all_odd_from_0_to_100.py
@@ -1,10 +1,10 @@
 # Prog08: Create a program that print all the odd numbers starting from 0 to 100. (Use while loop)
 
 # print 0 to 100 using while loop
-number = 1
+    #initialize variable
+num = 1 
 
-while number < 101:
-    print(number)
-    number += 2
-# check if odd
-# print output
+while num < 101:   # loop until less than 101
+    print(num)     # print output
+    num += 2       # increases number by 2 to get the next odd number
+    
