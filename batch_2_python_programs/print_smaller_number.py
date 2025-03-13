cf4057829a5537192b4366b602110088562da84c
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-13 11:54:35 +0800

    ask inputs

--- batch_2_python_programs/print_smaller_number.py
+++ batch_2_python_programs/print_smaller_number.py
@@ -1,5 +1,9 @@
 # Prog01: Create a program that ask user to input 2 numbers. Print the smaller number.
 
 # ask user for inputs of two numbers
+
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter a number: "))
+
 # check which is the smaller number
 # print the smaller number
