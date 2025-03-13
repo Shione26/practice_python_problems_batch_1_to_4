8d090997d0f9fcf0adf72244310e32fa41c2a1ee
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-13 20:34:28 +0800

    print 0-100

--- batch_1_python_programs/print_0_to_100_except_ending_0.py
+++ batch_1_python_programs/print_0_to_100_except_ending_0.py
@@ -1,5 +1,9 @@
 # Prog10: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero.
 
 # print 0-100
+
+for number in range(1, 101):
+    print (number)
+
 # check condition if number is ending in zero
 # print output
