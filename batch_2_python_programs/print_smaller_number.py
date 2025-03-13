caf618a4700cd241fd09467c54ee77000d3e159a
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-13 11:53:26 +0800

    pseudocode

---
+++ batch_2_python_programs/print_smaller_number.py
@@ -0,0 +1,5 @@
+# Prog01: Create a program that ask user to input 2 numbers. Print the smaller number.
+
+# ask user for inputs of two numbers
+# check which is the smaller number
+# print the smaller number
