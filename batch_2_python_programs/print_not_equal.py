e8e750d8ff06e581027b827a0dd4a49d83bcdb8c
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-13 15:05:47 +0800

    ask inputs

--- batch_2_python_programs/print_not_equal.py
+++ batch_2_python_programs/print_not_equal.py
@@ -1,5 +1,8 @@
 # Prog02: Create a program that ask user to input 2 numbers. Print "Not Equal" when the numbers are not the same.
 
 # ask user for inputs of two numbers
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter a number: "))
+
 # check if the two numbers are not the same
 # print not equal
