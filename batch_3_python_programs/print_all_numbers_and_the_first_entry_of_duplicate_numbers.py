8c57e590731a25f3ade1a3b4611402d8e24a7f34
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-13 22:03:11 +0800

    pseudocode

---
+++ batch_3_python_programs/print_all_numbers_and_the_first_entry_of_duplicate_numbers.py
@@ -0,0 +1,7 @@
+# Prog02: Create a program that ask user to input 10 numbers. Display all numbers. For numbers with duplicate, display only the first entry.
+
+# ask user to input 10 numbers
+# use a list
+# display all numbers
+    # check if numbers have duplicate
+    # display only the first entry
