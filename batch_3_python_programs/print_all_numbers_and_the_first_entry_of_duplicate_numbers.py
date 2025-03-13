8695ff777a904bb67bc0bfd10fd62c4c52b05352
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-13 22:05:29 +0800

    ask for 10 inputs

--- batch_3_python_programs/print_all_numbers_and_the_first_entry_of_duplicate_numbers.py
+++ batch_3_python_programs/print_all_numbers_and_the_first_entry_of_duplicate_numbers.py
@@ -1,6 +1,10 @@
 # Prog02: Create a program that ask user to input 10 numbers. Display all numbers. For numbers with duplicate, display only the first entry.
 
 # ask user to input 10 numbers
+
+for number_count in range(10):
+    number = int(input(f"Enter number {number_count +1}: "))
+
 # use a list
 # display all numbers
     # check if numbers have duplicate
