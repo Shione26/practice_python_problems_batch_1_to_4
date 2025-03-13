0d41f33a361fdedc22fcead5914a5dfdde54830b
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-13 11:36:52 +0800

    ask inputs

--- print_numbers_between_two_numbers.py
+++ print_numbers_between_two_numbers.py
@@ -1,5 +1,8 @@
 # Prog10: Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers.
 
 # ask user for inputs of two numbers
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter a number: "))
+
 # get all numbers in between two numbers
 # print output
