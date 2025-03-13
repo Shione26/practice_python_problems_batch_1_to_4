fdb49bdb1e8dfaefc3d8be5eb01623e40126a0b7
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-13 11:32:14 +0800

    print output

--- print_all_even_from_0_to_100.py
+++ print_all_even_from_0_to_100.py
@@ -2,6 +2,7 @@
 
 # print numbers 1 to 100
 for number in range(0, 101):
-    print(number)
+
 # check if even number
-    # print if true
+    if number % 2 == 0:
+            print(number)     # print if true
