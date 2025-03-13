2d2582e980f3dae91d6a68ea8efb275564e4af52
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-13 15:10:07 +0800

    pseudocode

---
+++ batch_2_python_programs/print_difference.py
@@ -0,0 +1,5 @@
+# Prog03: Create a program that ask user to input 2 numbers. Print the difference of the two numbers.
+
+# ask the user for inputs of 2 numbers
+# subtract the two numbers
+# print the difference
