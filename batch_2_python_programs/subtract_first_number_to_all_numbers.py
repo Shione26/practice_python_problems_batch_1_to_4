abcd9c9d457c7967ac963e1df378a630a4b4b178
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-13 18:20:31 +0800

    pseudocode

---
+++ batch_2_python_programs/subtract_first_number_to_all_numbers.py
@@ -0,0 +1,7 @@
+# Prog06: Create a program that ask user to input 10 numbers. Print the result of the first number minus all of the remaining numbers.
+
+# ask user for 10 imputs
+    # initialize variable 
+    # loop to ask user for 10 inputs
+    # subtract every inputted number from the difference in each loop iteration
+# print the result
