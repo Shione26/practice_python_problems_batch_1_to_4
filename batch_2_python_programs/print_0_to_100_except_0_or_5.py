3016e6f2c7ab90de49f059b51359469fb6255152
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-13 20:21:56 +0800

    pseudocode

---
+++ batch_2_python_programs/print_0_to_100_except_0_or_5.py
@@ -0,0 +1,5 @@
+# Prog09: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero or ending five.
+
+# print 0-100
+# check condition if ending zero or 5
+# print output
