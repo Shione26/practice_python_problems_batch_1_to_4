06f5160ebd2f7d87a580ae8445cb2b9e9f259552
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-14 15:32:20 +0800

    ask inputs

--- batch_4_python_programs/print_the_most_number_of_duplicate.py
+++ batch_4_python_programs/print_the_most_number_of_duplicate.py
@@ -1,7 +1,12 @@
 # Prog02: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number with the most number of duplicate.
 
 # create an empty list
+input_numbers = []
+
 # loop to ask user to input a number continuesly until invalid input
+while True:
+    user_input = input("Enter a number: ")
+    
 # check if input is valid
 # if yes, store input to the list
 # if no, break
