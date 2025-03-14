43cfde183b28f09364394eb4ed5c73ab0935f71c
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-14 16:07:31 +0800

    ask inputs

--- batch_4_python_programs/print_average.py
+++ batch_4_python_programs/print_average.py
@@ -1,8 +1,14 @@
 # Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the average.
 
 # create an empty list
+input_numbers = []
+
 # loop to ask user to input a number continuesly until invalid input
+while True:
+    user_input = (input("Enter a number: "))
+
 # check if input is valid
+
 # if yes, store input to the list
 # if no, break
 # get their sum then divide to the length of the input
