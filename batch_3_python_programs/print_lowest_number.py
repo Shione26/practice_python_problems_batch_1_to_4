9b3ac087055653b2d6a50371f7e1fd68c0c4029f
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-14 14:22:32 +0800

    pseudocode

---
+++ batch_3_python_programs/print_lowest_number.py
@@ -0,0 +1,8 @@
+# Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the lowest number
+
+# create an empty list
+# loop to ask user to input a number continuesly until invalid input
+# check if input is invalid
+    # if no, break
+    # if yes, add the input to the list
+# print the lowest number using min() function
