9505b761f93ea459346b6ce33c417d6c47b46660
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-14 15:08:49 +0800

    ask inputs

--- batch_4_python_programs/print_numbers_with_duplicate.py
+++ batch_4_python_programs/print_numbers_with_duplicate.py
@@ -1,6 +1,12 @@
 # Prog01: Create a program that ask user to input 10 numbers. Display all numbers that have duplicate.
 
 # create a list
+numbers = []
+
 # ask the user for 10 inputs
-# identify which numbers have dupplicate
+for number_count in range(10):
+    number = int(input(f"Enter number {number_count + 1}: "))
+    numbers.append(number)
+
+# identify which numbers have duplicate
 # print output
