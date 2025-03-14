43ceec563ac19a39d0d70ba4d2bcdfbf30497e42
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-14 14:24:10 +0800

    ask the user for inputs

--- batch_3_python_programs/print_lowest_number.py
+++ batch_3_python_programs/print_lowest_number.py
@@ -1,7 +1,11 @@
 # Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the lowest number
 
 # create an empty list
+input_numbers = []
+
 # loop to ask user to input a number continuesly until invalid input
+while True:
+    user_input = input("Enter a number: ")
 # check if input is invalid
     # if no, break
     # if yes, add the input to the list
