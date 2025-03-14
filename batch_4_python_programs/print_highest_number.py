fad465c8bbba2f34196caf99c2cd53f824b2a5f4
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-14 15:41:35 +0800

    pseudocode

---
+++ batch_4_python_programs/print_highest_number.py
@@ -0,0 +1,8 @@
+# Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the highest number
+
+# create an empty list
+# loop to ask user to input a number continuesly until invalid input
+# check if input is valid
+# if yes, store input to the list
+# if no, break
+# print the highest number using max() function
