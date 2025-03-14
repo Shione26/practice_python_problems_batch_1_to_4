1bd721401227a213d35b15585c5b82daa385a560
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-14 14:59:49 +0800

    added restrictions and edited the list

--- batch_3_python_programs/sort_from_lowest_to_higherst.py
+++ batch_3_python_programs/sort_from_lowest_to_higherst.py
@@ -1,15 +1,21 @@
 # Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from lowest to highest. Clue: sort() function
 
 # create an empty list
-input_numbers = set()
+input_numbers = []
 
 # loop to ask user to input a number continuesly until invalid input
 while True:
     user_input = (input("Enter a number: "))
-    
+
 # check if input is valid
-    # if yes, store input to the list
-    # if no, break
+    try:
+        user_input = int(user_input)
+        input_numbers.append(user_input)    # if yes, store input to the list
+    except ValueError:               
+        print("Input is invalid")           # if no, break
+        break
+
+
 # sort numbers from lowest to highest using sort() function
 # print output
 
