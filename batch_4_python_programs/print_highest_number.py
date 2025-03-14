4a1a6e2d3d605b9cd7da9d0c096253c8bc42c22d
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-14 15:45:25 +0800

    check validation

--- batch_4_python_programs/print_highest_number.py
+++ batch_4_python_programs/print_highest_number.py
@@ -6,8 +6,13 @@ input_numbers = []
 # loop to ask user to input a number continuesly until invalid input
 while True:
     user_input = input("Enter a number: ")
-    
+
 # check if input is valid
-# if yes, store input to the list
-# if no, break
+    try:
+        user_input = int(user_input)
+        input_numbers.append(user_input)   # if yes, store the input to the list
+    except ValueError:
+        print("Invalid input")      
+        break      # if no, break
+
 # print the highest number using max() function
