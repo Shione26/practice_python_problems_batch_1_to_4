91594983b6cd57e666a1022ba595f4babe4dbf4e
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-14 15:30:11 +0800

    pseudocode

---
+++ batch_4_python_programs/print_the_most_number_of_duplicate.py
@@ -0,0 +1,9 @@
+# Prog02: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number with the most number of duplicate.
+
+# create an empty list
+# loop to ask user to input a number continuesly until invalid input
+# check if input is valid
+# if yes, store input to the list
+# if no, break
+# find which has the most number of duplicates
+# print output
