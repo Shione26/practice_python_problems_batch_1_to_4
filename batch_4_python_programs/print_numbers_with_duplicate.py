f1504d94179a93161eb532ebe1fa71b477241ce9
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-14 15:07:01 +0800

    pseudocode

---
+++ batch_4_python_programs/print_numbers_with_duplicate.py
@@ -0,0 +1,6 @@
+# Prog01: Create a program that ask user to input 10 numbers. Display all numbers that have duplicate.
+
+# create a list
+# ask the user for 10 inputs
+# identify which numbers have dupplicate
+# print output
