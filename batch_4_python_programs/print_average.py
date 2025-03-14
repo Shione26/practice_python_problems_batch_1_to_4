129407d02cb17b08f9dcc37a760b1de9986854fa
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-14 16:06:18 +0800

    pseudocode

---
+++ batch_4_python_programs/print_average.py
@@ -0,0 +1,9 @@
+# Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the average.
+
+# create an empty list
+# loop to ask user to input a number continuesly until invalid input
+# check if input is valid
+# if yes, store input to the list
+# if no, break
+# get their sum then divide to the length of the input
+# print output
